26dadc76565d8a68f23433348e3b1cf12e777c5f
tristanfrench <tw15220@my.bristol.ac.uk>
2018-10-27 13:00:19 +0200

    reads video

--- readVid.py
+++ readVid.py
@@ -1,16 +1,22 @@
 import numpy as np
 import cv2
+import time
 
-cap = cv2.VideoCapture('collectCircleTapRand_09041010/video_1.mp4')
-
+cap = cv2.VideoCapture("collectCircleTapRand_09041010/video_1.mp4")
+i=0
 while(cap.isOpened()):
     ret, frame = cap.read()
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.waitKey(1) #so you can see the video
+       
+    if ret != 0:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    if ret == 0: #False if video is finished
+        cap.release()
         break
+    cv2.imshow("frame",gray)
+    i+=1
+    
 
-cap.release()
-cv2.destroyAllWindows()
+
+#cv2.destroyAllWindows()
