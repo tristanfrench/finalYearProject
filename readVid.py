9c54df6b5f0ede4aa7bc83463b2a6f98bbf5cf14
tristanfrench <tw15220@my.bristol.ac.uk>
2018-11-05 18:37:40 +0000

    data done

--- readVid.py
+++ readVid.py
@@ -3,6 +3,7 @@ import cv2
 import csv
 import time
 import processImg 
+import pandas as pd
 import os
 from os import listdir
 from os.path import isfile, join
@@ -33,6 +34,7 @@ def readVideo(mainFolder,videoFolder,vid,write=False):
             cap.release()
             break
         i+=1
+    '''
     if write == True:
         it=0
         differenceArgs = processImg.getArgmax(differencePerFrame)
@@ -42,30 +44,26 @@ def readVideo(mainFolder,videoFolder,vid,write=False):
             if it in differenceArgs:
                 if ret != 0:
                     currentImg = cv2.cvtColor(currentFrame, cv2.COLOR_BGR2GRAY)
-                    outfile = "%s/extractedImages/%s_%s.jpg" %(mainFolder,vid[:-4],it)
-                    print(outfile)
+                    outfile = "%s/extractedImages/%s_%s.jpg" %(mainFolder,format_string(vid[:-4]),it)
                     cv2.imwrite(outfile,currentImg)
             if ret == 0: # 0 if video is finished
                 cap.release()
                 break
             it+=1
-
+    '''
     return differencePerFrame
     
 
-def readCsv():
-    with open('video_targets.csv', mode='r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        line_count = 0
-        for row in csv_reader:
-            #if line_count == 0:
-                #print(f'Column names are {", ".join(row)}')
-                #line_count += 1
-            #print(row["pose_1"],row["pose_6"])
-            line_count += 1
-        #print(f'Processed {line_count} lines.')
+def readCsv(csv_file):
+    data = pd.read_csv(csv_file)
+    newData = data[['pose_1','pose_6']]
+    return newData
 
-
+def format_string(s):
+    if len(s) != 10:
+        for i in range(0,10-len(s)):
+            s = s[:6] + '0' + s[6:] 
+    return s
 
 
 def main():
@@ -73,10 +71,12 @@ def main():
     videoDir = mainFolder+'/videos/'
     myPath = os.getcwd()+'/'+videoDir
     allVideos = [f for f in listdir(myPath) if isfile(join(myPath, f))]
-
-    #for vid in allVideos[0:2]:
-        #result = readVideo(mainFolder,videoDir,vid,write=True)
-    readCsv()
+    it = 0
+    for vid in allVideos:
+        it+=1
+        result = readVideo(mainFolder,videoDir,vid,write=False)
+        print(it/20,'%')
+    #readCsv('video_targets.csv')
     
 
 if __name__ == '__main__':
