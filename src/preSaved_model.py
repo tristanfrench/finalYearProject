65f63b6be33d0d2f53cac9d4e40bb045fd221225
tristanfrench <tw15220@my.bristol.ac.uk>
2019-02-20 14:49:30 +0000

    own occlusion function

--- src/preSaved_model.py
+++ src/preSaved_model.py
@@ -13,10 +13,10 @@ import matplotlib.image as mpimg
 #import math
 from matplotlib.pyplot import imshow
 from keras.callbacks import TensorBoard
-from vis.visualization import visualize_activation
-from vis.utils import utils
+#from vis.visualization import visualize_activation
+#from vis.utils import utils
 from keras import activations
-from vis.visualization import visualize_saliency
+#from vis.visualization import visualize_saliency
 
 def iter_occlusion(image, size=8):
     # taken from https://www.kaggle.com/blargl/simple-occlusion-and-saliency-maps
@@ -43,29 +43,60 @@ def iter_occlusion(image, size=8):
 
             yield x - occlusion_padding, y - occlusion_padding, \
                 tmp[occlusion_padding:tmp.shape[0] - occlusion_padding, occlusion_padding:tmp.shape[1] - occlusion_padding]
+def single_square_occ(img,size=10):
+    img_y,img_x = np.shape(img)
+    nb_iter = int(img_y/size)
+    colour_value = 150
+    for row in range(0,nb_iter):
+        for col in range(0,nb_iter):
+            tmp = img.copy()
+            y_0 = row*size
+            y_1 = (row+1)*size 
+            x_0 = col*size
+            x_1 = (col+1)*size
+            tmp[y_0:y_1,x_0:x_1] = colour_value
+
+            yield tmp,y_0,y_1,x_0,x_1
+
+
+
 def main():
     model = load_model('kersa_1.h5')
-    img = plt.imread("cropSampled/video_0099_8_crop.jpg")
-    img_occ = img[:][:,:,0]
-    img_occ.setflags(write=1)
-    #img_occ[6:10,100:104] = 0
-    #plt.imshow(img_occ,cmap='gray')
+    img = plt.imread("cropSampled/video_1608_10_crop.jpg")[:,:,0]
+
+    #img_occ = img[:][:,:,0]
+    #img_occ.setflags(write=1)
+    #img_occ[0:50,:] = 0
+    fig, ax = plt.subplots()
+    ax.imshow(img,cmap='gray')
     #plt.show()
-    X = img_occ.reshape(1, 160,160, 1)
-    out = model.predict(X)[[0]]
+    #X = img_occ.reshape(1, 160,160, 1)
+    #out = model.predict(X)[[0]]
     #print(out)
     # occlusion
     img_size = 160
-    occlusion_size = 4
+    occlusion_size = 10
     label_99 = 2.544006547
+    label_1608 = -0.152099177
+
     heatmap = np.zeros((img_size, img_size), np.float32)
+    '''
     for n, (x, y, img_float) in enumerate(iter_occlusion(img, size=occlusion_size)):
-        X = img_float[:,:,0].reshape(1, 160,160, 1)
-        out = model.predict(X)[[0]]
-        heatmap[y:y + occlusion_size, x:x + occlusion_size] = abs(out-label_99)
-    plt.imshow(heatmap)
-    plt.colorbar()
+
+    '''
+    for occ_img,y_0,y_1,x_0,x_1 in single_square_occ(img):
+        
+        x = occ_img.reshape(1, 160,160, 1)
+        out = model.predict(x)[[0]]
+        heatmap[y_0:y_1,x_0:x_1] = abs(out-label_1608)
+    ax.imshow(heatmap,alpha=0.6)
+    #fig.colorbar(heatmap)
     plt.show()
+    
+    #print(heatmap)
+    #print(heatmap[:2,::10])
+    print(np.max(heatmap),np.min(heatmap))
+
 
 
 
