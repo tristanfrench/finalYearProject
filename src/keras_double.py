e338b49a346a0e805b7e5617a5c9733860987546
Tristan Winstanley <tw15220@my.bristol.ac.uk>
2019-03-25 22:42:40 +0000

    standardization"

--- src/keras_double.py
+++ src/keras_double.py
@@ -20,23 +20,26 @@ import sys
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_integer('batch_size', 64, 'Number of examples per mini-batch (default: %(default)d)')
 tf.app.flags.DEFINE_integer('max_epochs', 1,'Number of mini-batches to train on. (default: %(default)d)')
-tf.app.flags.DEFINE_integer('normalize', 1,'Normalise data or not(default: %(default)d)')
-class Label():
+tf.app.flags.DEFINE_integer('normalize', 0,'Normalise data or not(default: %(default)d)')
+class Label:
     def __init__(self, x):
         self.x = x
-        self.x_min = np.min(x)
-        self.x_max = np.max(x)
+        self.x_mean = np.mean(x)
+        self.x_std = np.max(x)
 
     def norm(self):
         '''
         Normalize whole array, min max normalization
         '''
-        return [(i-self.x_min)/(self.x_max-self.x_min) for i in self.x]
+        return [(i-self.x_mean)/(self.x_std) for i in self.x]
     def __repr__(self):
         return self.x
 
-def inverse_norm(self, x, x_max, x_min):
-    return [i*(x_max-x_min)+x_min for i in x]
+def inverse_norm(x, x_mean, x_std):
+    try:
+        return [i*(x_std) + x_mean for i in x]
+    except:
+        return x*(x_std) + x_mean
 def image_preprocess(image_dir, label_dir):
     '''
     Returns image names and labels.
@@ -46,23 +49,25 @@ def image_preprocess(image_dir, label_dir):
     ground_truth = pd.read_csv("video_targets_minus1.csv")
     r = ground_truth['pose_1'].values
     theta = ground_truth['pose_6'].values
-    #instantiate class
-    r_class = Label(r)
-    theta_class = Label(theta)
-    #normalize data
-    if FLAGS.normalize:
-        r = r_class.norm
-        theta = theta_class.norm
     #define matrix containing angle and dislacement as columns
     labels = np.zeros([len(r),2])
-    labels[:,0] = r
-    labels[:,1] = theta
+    #instantiate class
+    r = Label(r)
+    theta = Label(theta)
+    
+    #normalize data
+    if FLAGS.normalize:
+        labels[:,0] = r.norm()
+        labels[:,1] = theta.norm()
+    else:
+        labels[:,0] = r.x
+        labels[:,1] = theta.x
     #duplicate each label 5 times because we use 5 images from each video which all have the same label
     n_duplicates = 5
     labels=[i for i in labels for _ in np.arange(n_duplicates)] 
     #images
     all_image_names = [image_dir+img_name for img_name in listdir(os.getcwd()+'/'+image_dir)]
-    return all_image_names, labels
+    return all_image_names, labels, r, theta
 
 def img_generator(filenames, labels, batch_size):
     '''
@@ -87,7 +92,7 @@ def main(argv):
     #Load all data
     image_dir = 'cropSampled/'
     label_dir = 'video_targets_minus1.csv'
-    images, labels = image_preprocess(image_dir, label_dir)
+    images, labels, r, theta = image_preprocess(image_dir, label_dir)
     #shuffle all data, labels and images shuffled in same way by keeping same seed
     rng = np.random.randint(1000)
     np.random.seed(rng)
@@ -124,31 +129,30 @@ def main(argv):
     model.add(Dense(2,name="preds"))
     #optimizer and loss
     model.compile(optimizer='adam', loss='mse', metrics=['mae'])
-
     #create generators
     train_generator = img_generator(train_data_images,train_data_labels,FLAGS.batch_size)
     val_generator = img_generator(val_data_images,val_data_labels,FLAGS.batch_size)
     test_generator = img_generator(test_data_images,test_data_labels,FLAGS.batch_size)
     #define logs directory for tensorboard
-    tensorboard = TensorBoard(log_dir="logs/keras_runs")
+    #tensorboard = TensorBoard(log_dir="logs/keras_runs")
     #define steps
     steps_per_epoch = math.ceil(len(train_data_images)/FLAGS.batch_size)
     val_steps = math.ceil(len(val_data_images)/FLAGS.batch_size)
     #Training
-    model.fit_generator(train_generator, steps_per_epoch=steps_per_epoch, epochs=FLAGS.max_epochs, validation_data=val_generator, validation_steps=val_steps, verbose=1, callbacks=[tensorboard])
+    model.fit_generator(train_generator, steps_per_epoch=2, epochs=FLAGS.max_epochs, validation_data=val_generator, validation_steps=2, verbose=1)#, callbacks=[tensorboard])
     #Evaluation
     test_steps = len(test_data_images)/FLAGS.batch_size
-    print(model.evaluate_generator(test_generator, steps=test_steps))
-    print(model.metrics_names)
+    #print(model.evaluate_generator(test_generator, steps=test_steps))
+    #print(model.metrics_names)
     
     #model.save(f'trained_models/kerasdouble_{argv[0]}_{FLAGS.max_epochs}_second.h5')
     
     #check a prediction:
-    img_to_see = plt.imread("cropSampled/video_1794_8_crop.jpg")[:,:,0]
+    img_to_see = plt.imread("cropSampled/video_1794_8_crop.jpg")[:,:,0]#1.5 35.2
     X = img_to_see.reshape(1, 160,160, 1)
     out = model.predict(X)[0]
-    inverse_norm(out,)
     print(out) 
+    print(inverse_norm(out[0], r.x_mean, r.x_std), inverse_norm(out[1], theta.x_mean, theta.x_std))
     print(FLAGS.normalize,'norm')
     
     
